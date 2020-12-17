6cbaaa38a5e7acfd397d448819f2a7cd7faa0fac
Ross Gardiner <github@public.rossng.eu>
2020-12-17 05:09:23 +0000

    Working instructions for setting up OpenShot infra

--- aws/README.md
+++ aws/README.md
@@ -36,7 +36,44 @@ The stack `<prefix>-openshot` deploys the infrastructure required for the OpenSh
 1. Use AWS Certificate Manager to generate a certificate for a domain of your choice where the OpenShot Cloud API will be hosted.
    - Make a note of the ARN of the certificate.
 1. Run `cdk deploy <prefix>-openshot` to deploy the OpenShot stack to AWS.
-1. Find the deployed EC2 instance in the AWS control panel. Connect to it
+
+### Connecting to the OpenShot instance for manual setup
+
+The OpenShot AWS infrastructure is structured to ensure that the OpenShot instance is only accessible via HTTPS on port 443. To achieve this, we put the OpenShot instance into an private VPC subnet. We then spin up an Application Load Balancer and use it to proxy the HTTP (port 80) endpoint on the OpenShot instance to a public HTTPS (port 443) endpoint.
+
+However, we also need to access the OpenShot instance via SSH for some manual configuration tasks. To do this, we spin up a 'bastion host' in the public subnet. This is a small instance that simply acts as an SSH jump box into the private subnet.
+
+Instead of keeping a permanent list of known keys on the bastion host, we use EC2 Instance Connect to connect to the bastion and then SSH Agent Forwarding to use our pre-created key pair (see above) to jump into the OpenShot instance.
+
+We won't detail SSH Agent Forwarding in much detail here. The recommended setup for Windows users is to use KeePass and KeeAgent as an SSH agent, and Windows' built-in OpenSSH as SSH client. PuTTY will also work.
+
+1. Generate a temporary key pair by running `ssh-keygen -t rsa -f temp`
+1. Push the temporary public key to the bastion host by running `aws ec2-instance-connect send-ssh-public-key --instance-id <bastion host instance ID> --availability-zone <e.g. eu-west-1a> --instance-os-user ec2-user --ssh-public-key file://temp.pub`
+   - The instance can be retrieved either from the EC2 console or from the CDK output
+1. Now you have 60 seconds to connect to the bastion host before the public key is erased. Run `ssh -i temp ec2-user@<bastion host DNS name> -A`
+   - `-A` enables agent forwarding
+   - The host DNS name can be retrieved either from the EC2 console or from the CDK output
+1. Last, we can connect to the OpenShot instance itself. Run `ssh ubuntu@<OpenShot instance private DNS>` at the bastion host terminal.
+   - The OpenShot instance private DNS name can be retrieved either from the EC2 console or from the CDK output
+
+### Setting up the OpenShot instance
+
+To set up the OpenShot instance, we need to run through a wizard at the terminal. Once you are connected to the instance, run `config-openshot-cloud` [as per the OpenShot Cloud API documentation](https://cloud.openshot.org/doc/getting_started.html).
+
+Choose the following options:
+
+- Role: both
+- Cloud username: cloud-admin
+- Cloud password: choose a secure password and make a note of it
+- AWS Access Key ID: `OpenShotAccessKeyId` from the CDK output
+- AWS Secret Access Key: `OpenShotSecretAccessKey` from the CDK output
+- AWS SQS Queue: `OpenShotAPIExportQueueName` from the CDK output
+- AWS Region: the region you are deploying to (e.g. `eu-west-1`)
+- Cloud API Public URL: `OpenShotLoadBalancerDnsName` from the CDK output
+
+You can test that the instance is running by going to `https://<load balancer DNS name>/doc/`. Just accept the certificate error for this test.
+
+Finally, point the domain for which you generated the certificate at the load balancer. This step will vary depending on how your DNS is set up.
 
 ## Useful commands
 
@@ -49,9 +86,10 @@ The stack `<prefix>-openshot` deploys the infrastructure required for the OpenSh
 
 ## AWS Configuration
 
-| Key                        | Value                                                                                                  |
-| -------------------------- | ------------------------------------------------------------------------------------------------------ |
-| clowdr/stackPrefix         | Prefix to be prepended to stack names (choose a unique name for each development environment)          |
-| clowdr/region              | Name of the AWS region to deploy to (e.g. eu-west-1)                                                   |
-| clowdr/account             | ID of the AWS account to deploy to                                                                     |
-| clowdr/openShotKeyPairName | Name of the key pair you created earlier. Will be used for SSH access to the OpenShot API EC2 instance |
+| Key                           | Value                                                                                                  |
+| ----------------------------- | ------------------------------------------------------------------------------------------------------ |
+| clowdr/stackPrefix            | Prefix to be prepended to stack names (choose a unique name for each development environment)          |
+| clowdr/region                 | Name of the AWS region to deploy to (e.g. eu-west-1)                                                   |
+| clowdr/account                | ID of the AWS account to deploy to                                                                     |
+| clowdr/openShotKeyPairName    | Name of the key pair you created earlier. Will be used for SSH access to the OpenShot API EC2 instance |
+| clowdr/openShotCertificateArn | ARN of the certificate you created for your domain that will host the OpenShot API.                    |
--- aws/lib/openshot-stack.ts
+++ aws/lib/openshot-stack.ts
@@ -1,5 +1,11 @@
+import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as ec2 from "@aws-cdk/aws-ec2";
+import {
+    GatewayVpcEndpointAwsService,
+    InterfaceVpcEndpointAwsService,
+} from "@aws-cdk/aws-ec2";
 import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
+import * as elbv2_targets from "@aws-cdk/aws-elasticloadbalancingv2-targets";
 import * as iam from "@aws-cdk/aws-iam";
 import * as sqs from "@aws-cdk/aws-sqs";
 import * as cdk from "@aws-cdk/core";
@@ -30,17 +36,94 @@ export class OpenshotStack extends cdk.Stack {
         );
 
         const vpc = new ec2.Vpc(this, "VPC", {
-            cidr: "10.1.0.0/16",
+            cidr: "10.16.0.0/23",
+            enableDnsHostnames: true,
+            enableDnsSupport: true,
             subnetConfiguration: [
                 {
                     name: "public",
                     subnetType: ec2.SubnetType.PUBLIC,
                 },
                 {
+                    cidrMask: 26,
                     name: "private",
                     subnetType: ec2.SubnetType.PRIVATE,
                 },
             ],
+        });
+
+        const vpcEndpointSecurityGroup = new ec2.SecurityGroup(
+            this,
+            "EndpointSecurityGroup",
+            {
+                vpc,
+                allowAllOutbound: true,
+            }
+        );
+
+        vpc.addGatewayEndpoint("S3Gateway", {
+            service: GatewayVpcEndpointAwsService.S3,
+        });
+
+        vpc.addInterfaceEndpoint("SSMMessages", {
+            open: true,
+            privateDnsEnabled: true,
+            service: InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+            subnets: vpc.selectSubnets(),
+            securityGroups: [vpcEndpointSecurityGroup],
+        });
+
+        vpc.addInterfaceEndpoint("EC2Messages", {
+            open: true,
+            privateDnsEnabled: true,
+            service: InterfaceVpcEndpointAwsService.EC2_MESSAGES,
+            subnets: vpc.selectSubnets(),
+            securityGroups: [vpcEndpointSecurityGroup],
+        });
+
+        vpc.addInterfaceEndpoint("SSM", {
+            open: true,
+            privateDnsEnabled: true,
+            service: InterfaceVpcEndpointAwsService.SSM,
+            subnets: vpc.selectSubnets(),
+            securityGroups: [vpcEndpointSecurityGroup],
+        });
+
+        vpc.addInterfaceEndpoint("SQS", {
+            open: true,
+            privateDnsEnabled: true,
+            service: InterfaceVpcEndpointAwsService.SQS,
+            subnets: vpc.selectSubnets(),
+            securityGroups: [vpcEndpointSecurityGroup],
+        });
+
+        const bastionInstance = new ec2.BastionHostLinux(this, "BastionHost", {
+            vpc,
+            subnetSelection: vpc.selectSubnets({
+                subnetType: ec2.SubnetType.PUBLIC,
+            }),
+        });
+
+        bastionInstance.allowSshAccessFrom(ec2.Peer.anyIpv4());
+
+        const openShotAmi = new ec2.LookupMachineImage({
+            name:
+                "OpenShot Cloud API (1.2.3)-82b57f78-74b8-415f-9bff-1705102d8557-ami-0bcc970b7833289ec.4",
+        });
+
+        const openShotInstance = new ec2.Instance(this, "OpenShotInstance", {
+            machineImage: openShotAmi,
+            instanceType: ec2.InstanceType.of(
+                ec2.InstanceClass.T2,
+                ec2.InstanceSize.MEDIUM
+            ),
+            keyName: this.node.tryGetContext("clowdr/openShotKeyPairName"),
+            vpc,
+            allowAllOutbound: true,
+            vpcSubnets: vpc.selectSubnets({
+                subnetType: ec2.SubnetType.PRIVATE,
+            }),
+            securityGroup: vpcEndpointSecurityGroup,
         });
 
         const openShotLoadBalancer = new elbv2.ApplicationLoadBalancer(
@@ -55,64 +138,46 @@ export class OpenshotStack extends cdk.Stack {
             }
         );
 
-        const openShotAmi = new ec2.LookupMachineImage({
-            name:
-                "OpenShot Cloud API (1.2.3)-82b57f78-74b8-415f-9bff-1705102d8557-ami-0bcc970b7833289ec.4",
-        });
-
-        const openShotInstance = new ec2.Instance(this, "OpenShotInstance", {
-            machineImage: openShotAmi,
-            instanceType: ec2.InstanceType.of(
-                ec2.InstanceClass.T2,
-                ec2.InstanceSize.MEDIUM
-            ),
-            keyName: this.node.tryGetContext("clowdr/openShotKeyPairName"),
-            vpc: vpc,
-            allowAllOutbound: true,
-            vpcSubnets: vpc.selectSubnets({
-                subnetType: ec2.SubnetType.PRIVATE,
-            }),
-        });
-
         openShotInstance.connections.allowFrom(
             openShotLoadBalancer,
             ec2.Port.tcp(80)
         );
         openShotInstance.connections.allowFrom(
-            openShotLoadBalancer,
+            bastionInstance,
             ec2.Port.tcp(22)
         );
+
+        bastionInstance.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
+        bastionInstance.connections.allowTo(openShotInstance, ec2.Port.tcp(22));
 
         const openShotQueue = new sqs.Queue(this, "OpenShotAPIExportQueue", {});
         openShotQueue.grantConsumeMessages(openShotServiceUser);
         openShotQueue.grantPurge(openShotServiceUser);
         openShotQueue.grantSendMessages(openShotServiceUser);
 
-        // const httpsListener = openShotLoadBalancer.addListener(
-        //     "OpenShotHttpsListener",
-        //     {
-        //         port: 443,
-        //         open: true,
-        //     }
-        // );
+        const httpsListener = openShotLoadBalancer.addListener(
+            "OpenShotHttpsListener",
+            {
+                port: 443,
+                open: true,
+                certificates: [
+                    elbv2.ListenerCertificate.fromCertificateManager(
+                        acm.Certificate.fromCertificateArn(
+                            this,
+                            "OpenShotCertificate",
+                            this.node.tryGetContext(
+                                "clowdr/openShotCertificateArn"
+                            )
+                        )
+                    ),
+                ],
+            }
+        );
 
-        // httpsListener.addTargets("OpenShotAPIHttpTarget", {
-        //     port: 80,
-        //     targets: [new elbv2_targets.InstanceTarget(openShotInstance)],
-        // });
-
-        // const sshListener = openShotLoadBalancer.addListener(
-        //     "OpenShotSshListener",
-        //     {
-        //         port: 22,
-        //         open: true,
-        //     }
-        // );
-
-        // sshListener.addTargets("OpenShotAPISshTarget", {
-        //     port: 22,
-        //     targets: [new elbv2_targets.InstanceTarget(openShotInstance)],
-        // });
+        httpsListener.addTargets("OpenShotAPIHttpTarget", {
+            port: 80,
+            targets: [new elbv2_targets.InstanceTarget(openShotInstance)],
+        });
 
         // Openshot
         new cdk.CfnOutput(this, "OpenShotAccessKeyId", {
@@ -129,6 +194,18 @@ export class OpenshotStack extends cdk.Stack {
 
         new cdk.CfnOutput(this, "OpenShotAPIExportQueueName", {
             value: openShotQueue.queueName,
+        });
+
+        new cdk.CfnOutput(this, "BastionHostPublicDnsName", {
+            value: bastionInstance.instancePublicDnsName,
+        });
+
+        new cdk.CfnOutput(this, "BastionHostInstanceId", {
+            value: bastionInstance.instanceId,
+        });
+
+        new cdk.CfnOutput(this, "OpenShotInstancePrivateDnsName", {
+            value: openShotInstance.instancePrivateDnsName,
         });
     }
 }

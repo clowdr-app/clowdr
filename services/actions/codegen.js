b8b2deb9476ebe238de7c6e7c5c91384161208c2
Ed Nutting <github@ednutting.com>
2020-12-21 17:31:43 +0000

    Use env for actions service graphql server path for codegen

--- services/actions/codegen.js
+++ services/actions/codegen.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 module.exports = {
     schema: [
         {
-            "http://localhost:8080/v1/graphql": {
+            [`http${process.env.GRAPHQL_API_SECURE_PROTOCOLS ? "s" : ""}://${
+                process.env.GRAPHQL_API_DOMAIN
+            }/v1/graphql`]: {
                 headers: {
                     "X-Hasura-Admin-Secret": process.env.HASURA_ADMIN_SECRET,
                 },

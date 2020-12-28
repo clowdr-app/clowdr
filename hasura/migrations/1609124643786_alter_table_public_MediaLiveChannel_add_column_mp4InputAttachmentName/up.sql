c3d1470a94567c4a3ee2905ba869520cbb00fd06
Ross Gardiner <github@public.rossng.eu>
2020-12-28 03:53:00 +0000

    🗃 Add input attachment names to MediaLiveChannel

---
+++ hasura/migrations/1609124643786_alter_table_public_MediaLiveChannel_add_column_mp4InputAttachmentName/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."MediaLiveChannel" ADD COLUMN "mp4InputAttachmentName" text NOT NULL;
---
+++ hasura/migrations/1609124661549_alter_table_public_MediaLiveChannel_add_column_vonageInputAttachmentName/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."MediaLiveChannel" ADD COLUMN "vonageInputAttachmentName" text NOT NULL;

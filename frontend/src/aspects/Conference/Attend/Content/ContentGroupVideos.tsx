572cd6d8c72463e9ba829d44cab0bf3a2f0f9ec0
Ross Gardiner <github@public.rossng.eu>
2021-01-03 21:53:53 +0000

    📦 Add subtitles to content group page

--- frontend/src/aspects/Conference/Attend/Content/ContentGroupVideos.tsx
+++ frontend/src/aspects/Conference/Attend/Content/ContentGroupVideos.tsx
@@ -1,9 +1,11 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Spinner } from "@chakra-ui/react";
 import { assertIsContentItemDataBlob, VideoContentBlob } from "@clowdr-app/shared-types/build/content";
+import { WebVTTConverter } from "@clowdr-app/srt-webvtt";
 import AmazonS3URI from "amazon-s3-uri";
 import * as R from "ramda";
 import React, { useMemo, useState } from "react";
-import ReactPlayer from "react-player";
+import { useAsync } from "react-async-hook";
+import ReactPlayer, { Config, TrackProps } from "react-player";
 import { ContentGroupDataFragment, ContentType_Enum } from "../../../../generated/graphql";
 
 export function ContentGroupVideos({ contentGroupData }: { contentGroupData: ContentGroupDataFragment }): JSX.Element {
@@ -53,7 +55,7 @@ export function ContentGroupVideos({ contentGroupData }: { contentGroupData: Con
             });
     }, [contentGroupData.contentItems, selectedVideoId]);
     return (
-        <Flex justifyContent="center" background="gray.900" borderRadius={5} py={5}>
+        <Flex justifyContent="center" alignItems="center" background="gray.900" borderRadius={5} py={5} minH="80vh">
             {videoContentItems}
         </Flex>
     );
@@ -79,11 +81,56 @@ export function ContentGroupVideo({
         return `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`;
     }, [videoContentItemData.transcode?.s3Url]);
 
-    return (
-        <>
-            <Heading as="h3" fontSize={24} mb={4} color="gray.50">
-                {title}
-            </Heading>
+    const { result: subtitlesUrl, loading, error } = useAsync(async () => {
+        if (!videoContentItemData.subtitles["en_US"] || !videoContentItemData.subtitles["en_US"].s3Url) {
+            return undefined;
+        } else {
+            try {
+                const { bucket, key } = new AmazonS3URI(videoContentItemData.subtitles["en_US"].s3Url);
+                const s3Url = `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`;
+
+                const response = await fetch(s3Url);
+
+                if (!response.ok) {
+                    throw new Error(`Could not retrieve subtitles file: ${response.status}`);
+                }
+
+                const blob = await response.blob();
+
+                return await new WebVTTConverter(blob).getURL();
+            } catch (e) {
+                console.error("Failure while parsing subtitle location", e);
+            }
+        }
+    }, [videoContentItemData.subtitles["en_US"]]);
+
+    const subtitlesConfig = useMemo<Config | null>(() => {
+        if (loading) {
+            return null;
+        }
+        if (error || !subtitlesUrl) {
+            return {};
+        }
+        const track: TrackProps = {
+            kind: "subtitles",
+            src: subtitlesUrl,
+            srcLang: "en",
+            default: true,
+            label: "English",
+        };
+        return {
+            file: {
+                tracks: [track],
+            },
+        };
+    }, [error, loading, subtitlesUrl]);
+
+    const player = useMemo(() => {
+        // Only render the player once both the video URL and the subtitles config are available
+        // react-player memoizes internally and only re-renders if the url or key props change.
+        return !previewTranscodeUrl || !subtitlesConfig ? (
+            <Spinner />
+        ) : (
             <ReactPlayer
                 url={previewTranscodeUrl}
                 controls={true}
@@ -91,7 +138,17 @@ export function ContentGroupVideo({
                 height="auto"
                 onPlay={onPlay}
                 onPause={onPause}
+                config={subtitlesConfig}
             />
+        );
+    }, [onPause, onPlay, previewTranscodeUrl, subtitlesConfig]);
+
+    return (
+        <>
+            <Heading as="h3" fontSize={24} mb={4} color="gray.50">
+                {title}
+            </Heading>
+            {player}
         </>
     );
 }

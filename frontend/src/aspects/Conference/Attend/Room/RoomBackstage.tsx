2d54c5dbbca2ebff60e4660ae9b528e5f7fb9ce6
Ross Gardiner <github@public.rossng.eu>
2021-01-20 06:31:40 +0000

    Organise backstage rooms more intuitively

--- frontend/src/aspects/Conference/Attend/Room/RoomBackstage.tsx
+++ frontend/src/aspects/Conference/Attend/Room/RoomBackstage.tsx
@@ -1,12 +1,15 @@
-import { Badge, Box, Button, Divider, Heading, Text, useColorModeValue, useToken } from "@chakra-ui/react";
+import { gql } from "@apollo/client";
+import { Box, Button, Divider, Heading, Text, useColorModeValue, useToken } from "@chakra-ui/react";
 import { formatRelative } from "date-fns";
 import * as R from "ramda";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import {
     EventPersonDetailsFragment,
+    RoomBackstage_EventFragment,
     RoomMode_Enum,
     RoomPage_RoomDetailsFragment,
     RoomPage_RoomEventSummaryFragment,
+    useRoomBackstage_GetEventsQuery,
 } from "../../../../generated/graphql";
 import usePolling from "../../../Generic/usePolling";
 import useCurrentUser from "../../../Users/CurrentUser/useCurrentUser";
@@ -25,8 +28,24 @@ function isEventSoon(event: RoomPage_RoomEventSummaryFragment): boolean {
     const now = new Date().getTime();
     const startTime = Date.parse(event.startTime);
 
-    return now >= startTime - 3 * 60 * 1000 && now <= startTime;
+    return now >= startTime - 15 * 60 * 1000 && now <= startTime;
 }
+
+gql`
+    query RoomBackstage_GetEvents($eventIds: [uuid!]!) {
+        Event(where: { id: { _in: $eventIds } }) {
+            ...RoomBackstage_Event
+        }
+    }
+
+    fragment RoomBackstage_Event on Event {
+        id
+        contentGroup {
+            id
+            title
+        }
+    }
+`;
 
 export function RoomBackstage({
     backstage,
@@ -41,12 +60,27 @@ export function RoomBackstage({
     const [gray100, gray900] = useToken("colors", ["gray.100", "gray.900"]);
     const backgroundColour = useColorModeValue(gray100, gray900);
 
+    const { refetch } = useRoomBackstage_GetEventsQuery({ skip: true });
+    const [eventsData, setEventsData] = useState<readonly RoomBackstage_EventFragment[]>([]);
+
     // Lazy-render the backstage
     const [loaded, setLoaded] = useState<boolean>(false);
     useEffect(() => {
-        if (backstage && !loaded) {
-            setLoaded(true);
+        async function fn() {
+            if (backstage && !loaded) {
+                setLoaded(true);
+                try {
+                    const result = await refetch({
+                        eventIds: roomDetails.events.map((event) => event.id),
+                    });
+                    setEventsData(result.data.Event);
+                } catch (e) {
+                    console.error("Failed to fetch event data for backstage");
+                }
+            }
         }
+        fn();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [backstage, loaded]);
 
     const myEventPeople = useMemo(
@@ -61,8 +95,10 @@ export function RoomBackstage({
     const computeSortedEvents = useCallback(() => {
         setSortedEvents(
             R.sortWith(
-                [R.descend(isEventNow), R.descend(isEventSoon), R.ascend(R.prop("startTime"))],
-                roomDetails.events
+                [R.ascend(R.prop("startTime"))],
+                roomDetails.events.filter((event) =>
+                    [RoomMode_Enum.Presentation, RoomMode_Enum.QAndA].includes(event.intendedRoomModeName)
+                )
             )
         );
     }, [roomDetails.events]);
@@ -72,45 +108,6 @@ export function RoomBackstage({
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const [eventTemporalBadges, setEventTemporalBadges] = useState<{
-        [eventId: string]: JSX.Element;
-    }>({});
-
-    const updateEventTemporalBadges = useCallback(() => {
-        setEventTemporalBadges(
-            R.fromPairs(
-                sortedEvents
-                    .filter((event) =>
-                        [RoomMode_Enum.Presentation, RoomMode_Enum.QAndA].includes(event.intendedRoomModeName)
-                    )
-                    .map((event) => {
-                        if (isEventNow(event)) {
-                            return [
-                                event.id,
-                                <Badge key={`badge-${event.id}`} colorScheme="green">
-                                    Now
-                                </Badge>,
-                            ];
-                        }
-
-                        if (isEventSoon(event)) {
-                            return [
-                                event.id,
-                                <Badge key={`badge-${event.id}`} colorScheme="blue">
-                                    Soon
-                                </Badge>,
-                            ];
-                        }
-
-                        return [event.id, <></>];
-                    })
-            )
-        );
-    }, [sortedEvents]);
-
-    usePolling(updateEventTemporalBadges, 5000, true);
-    useEffect(updateEventTemporalBadges, [updateEventTemporalBadges]);
-
     const [now, setNow] = useState<Date>(new Date());
     const updateNow = useCallback(() => {
         setNow(new Date());
@@ -119,56 +116,97 @@ export function RoomBackstage({
 
     const [selectedEventId, setSelectedEventId] = useState<string | null>(null);
 
+    const makeEventEl = useCallback(
+        (event: RoomPage_RoomEventSummaryFragment) => {
+            const haveAccessToEvent = !!myEventPeople.find((eventPerson) => user && eventPerson.eventId === event.id);
+            const eventData = eventsData.find((x) => x.id === event.id);
+            return (
+                <Box key={event.id}>
+                    <Heading as="h4" size="md" textAlign="left" mt={5} mb={1}>
+                        {eventData?.contentGroup ? `${eventData.contentGroup.title} (${event.name})` : event.name}
+                    </Heading>
+
+                    <Text my={2} fontStyle="italic">
+                        {formatRelative(Date.parse(event.startTime), now)}
+                    </Text>
+
+                    {haveAccessToEvent ? (
+                        selectedEventId !== event.id ? (
+                            <Button colorScheme="green" onClick={() => setSelectedEventId(event.id)}>
+                                Open backstage room
+                            </Button>
+                        ) : (
+                            <></>
+                        )
+                    ) : (
+                        <>
+                            <Text>You do not have access to the backstage for this event.</Text>
+                            <HandUpButton currentRoomEvent={event} eventPeople={eventPeople} />
+                        </>
+                    )}
+                    {selectedEventId === event.id ? (
+                        <Box display={haveAccessToEvent ? "block" : "none"} mt={2}>
+                            <EventVonageRoom eventId={event.id} />
+                        </Box>
+                    ) : (
+                        <></>
+                    )}
+                    <Divider my={4} />
+                </Box>
+            );
+        },
+        [eventPeople, eventsData, myEventPeople, now, selectedEventId, user]
+    );
+
+    const [eventsNow, setEventsNow] = useState<readonly RoomPage_RoomEventSummaryFragment[]>([]);
+    const [eventsSoon, setEventsSoon] = useState<readonly RoomPage_RoomEventSummaryFragment[]>([]);
+    const [eventsOther, setEventsOther] = useState<readonly RoomPage_RoomEventSummaryFragment[]>([]);
+    const updateEventGroups = useCallback(() => {
+        setEventsNow(sortedEvents.filter(isEventNow));
+        setEventsSoon(sortedEvents.filter(isEventSoon));
+        setEventsOther(sortedEvents.filter((event) => !isEventNow(event) && !isEventSoon(event)));
+    }, [sortedEvents]);
+
+    usePolling(updateEventGroups, 5000, true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(updateEventGroups, []);
+
     const eventRooms = useMemo(
         () =>
             !loaded ? (
                 <></>
             ) : (
-                sortedEvents
-                    .filter((event) =>
-                        [RoomMode_Enum.Presentation, RoomMode_Enum.QAndA].includes(event.intendedRoomModeName)
-                    )
-                    .map((event) => {
-                        const haveAccessToEvent = !!myEventPeople.find(
-                            (eventPerson) => user && eventPerson.eventId === event.id
-                        );
-                        return (
-                            <Box key={event.id}>
-                                <Heading as="h4" size="md" textAlign="left" mt={5} mb={1}>
-                                    {event.name} {eventTemporalBadges[event.id]}
-                                </Heading>
-
-                                <Text my={2} fontStyle="italic">
-                                    {formatRelative(Date.parse(event.startTime), now)}
-                                </Text>
-
-                                {haveAccessToEvent ? (
-                                    selectedEventId !== event.id ? (
-                                        <Button colorScheme="green" onClick={() => setSelectedEventId(event.id)}>
-                                            Open backstage room
-                                        </Button>
-                                    ) : (
-                                        <></>
-                                    )
-                                ) : (
-                                    <>
-                                        <Text>You do not have access to the backstage for this event.</Text>
-                                        <HandUpButton currentRoomEvent={event} eventPeople={eventPeople} />
-                                    </>
-                                )}
-                                {selectedEventId === event.id ? (
-                                    <Box display={haveAccessToEvent ? "block" : "none"} mt={2}>
-                                        <EventVonageRoom eventId={event.id} />
-                                    </Box>
-                                ) : (
-                                    <></>
-                                )}
-                                <Divider my={4} />
-                            </Box>
-                        );
-                    })
+                <>
+                    {eventsNow.length > 0 ? (
+                        <Heading as="h4" size="lg" mt={5} textAlign="left">
+                            Events happening now
+                        </Heading>
+                    ) : undefined}
+                    {eventsNow.map((x) => (
+                        <Box key={x.id}>{makeEventEl(x)}</Box>
+                    ))}
+                    {eventsSoon.length > 0 ? (
+                        <Heading as="h4" size="lg" mt={5} textAlign="left">
+                            Events happening soon
+                        </Heading>
+                    ) : undefined}
+                    {eventsSoon.map((x) => (
+                        <Box key={x.id}>{makeEventEl(x)}</Box>
+                    ))}
+                    {eventsOther.length > 0 ? (
+                        <Heading as="h4" size="lg" mt={5} textAlign="left">
+                            Other events in this room
+                        </Heading>
+                    ) : undefined}
+                    {eventsOther.map((x) => (
+                        <Box key={x.id}>{makeEventEl(x)}</Box>
+                    ))}
+                    {eventsNow.length === 0 && eventsSoon.length === 0 && eventsOther.length === 0 ? (
+                        <>No events in this room</>
+                    ) : undefined}
+                </>
             ),
-        [eventPeople, eventTemporalBadges, loaded, myEventPeople, now, selectedEventId, sortedEvents, user]
+        [eventsNow, eventsOther, eventsSoon, loaded, makeEventEl]
     );
 
     return backstage ? (

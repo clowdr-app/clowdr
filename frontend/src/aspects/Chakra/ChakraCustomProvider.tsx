2bd1d43add08053de7b2ad5718d621da1aa54863
Ed Nutting <github@ednutting.com>
2021-03-27 23:28:46 +0000

    Reduce border radii a lot

--- frontend/src/aspects/Chakra/ChakraCustomProvider.tsx
+++ frontend/src/aspects/Chakra/ChakraCustomProvider.tsx
@@ -38,6 +38,17 @@ const theme = extendTheme({
     config: {
         initialColorMode: "dark",
     },
+    radii: {
+        none: "0",
+        sm: "0",
+        base: "0.0625rem",
+        md: "0.125rem",
+        lg: "0.25rem",
+        xl: "0.375rem",
+        "2xl": "0.5rem",
+        "3xl": "0.75rem",
+        full: "9999px",
+    },
     colors,
     shadows: {
         outline: defaultOutline_AsBoxShadow,

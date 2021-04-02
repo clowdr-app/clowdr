066d9f7cf2e93ed21a7cc5474248e2846e1df4e8
Ross Gardiner <github@public.rossng.eu>
2021-04-02 05:20:17 +0100

    Clean out old Vimeo code

--- services/actions/src/types/hasura/event.ts
+++ services/actions/src/types/hasura/event.ts
@@ -112,13 +112,6 @@ export interface CombineVideosJobData extends BaseData {
     message: string | null;
 }
 
-// export interface PublishVideoJobData extends BaseData {
-//     contentItemId: string;
-//     jobStatusName: JobStatus_Enum;
-//     conferenceId: string;
-//     vimeoVideoUrl: string | null;
-// }
-
 export interface BroadcastContentItemDataBase extends BaseData {
     contentItemId: string | null;
     eventId: string | null;

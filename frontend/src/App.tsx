02136c0e6ad4325b0a71d7af1f14721faa0b7491
Ed Nutting <github@ednutting.com>
2021-04-07 18:26:29 +0100

    Disable maintenance mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,6 @@ import ConferenceCurrentUserActivePermissionsProvider, {
 import { CurrentAttendeeProvider, useMaybeCurrentAttendee } from "./aspects/Conference/useCurrentAttendee";
 import EmojiMartProvider from "./aspects/Emoji/EmojiMartContext";
 import ForceUserRefresh from "./aspects/ForceUserRefresh/ForceUserRefresh";
-import DownForMaintenancePage from "./aspects/Maintenance/DownForMaintenancePage";
 import LeftSidebar from "./aspects/Menu/LeftSidebar";
 import MainMenu, { MenuBar } from "./aspects/Menu/MainMenu";
 import RightSidebar from "./aspects/Menu/RightSidebar";
@@ -32,13 +31,13 @@ function useQuery() {
 }
 
 export default function App(): JSX.Element {
-    const query = useQuery();
-    const bypassDfmMatch = query.get("bypassMaintenance");
-    console.info("bypassDfmMatch", bypassDfmMatch);
+    // const query = useQuery();
+    // const bypassDfmMatch = query.get("bypassMaintenance");
+    // console.info("bypassDfmMatch", bypassDfmMatch);
 
-    if (!bypassDfmMatch) {
-        return <DownForMaintenancePage />;
-    }
+    // if (!bypassDfmMatch) {
+    //     return <DownForMaintenancePage />;
+    // }
 
     return (
         <Switch>

41b2b01ca2b9f81e9d6d15bb368ad07afb3823f4
Ed Nutting <github@ednutting.com>
2021-04-07 17:46:28 +0100

    Adjust maintenance start time

--- frontend/src/aspects/Maintenance/DownForMaintenancePage.tsx
+++ frontend/src/aspects/Maintenance/DownForMaintenancePage.tsx
@@ -8,7 +8,7 @@ export default function DownForMaintenancePage(): JSX.Element {
             <GenericErrorPage heading="Offline for maintenance">
                 <VStack alignItems="flex-start" maxW={625}>
                     <Text fontSize="xl" lineHeight="revert" fontWeight="light">
-                        Clowdr is offline for maintenance starting 4:15pm UTC on 7th April for approximately 30 minutes.
+                        Clowdr is offline for maintenance starting 5pm UTC on 7th April for approximately 30 minutes.
                     </Text>
                     <Text fontSize="xl" lineHeight="revert" fontWeight="light">
                         This is while we make important updates to the platform to improve the chat and video

6665486715e728cbfcfcab593343821f7771c993
Ross Gardiner <github@public.rossng.eu>
2021-04-16 16:39:06 +0100

    Smooth microphone activity preview a bit

--- frontend/src/aspects/Conference/Attend/Room/Chime/MicrophoneActivityPreview.tsx
+++ frontend/src/aspects/Conference/Attend/Room/Chime/MicrophoneActivityPreview.tsx
@@ -3,14 +3,21 @@ import { useLocalAudioInputActivity } from "amazon-chime-sdk-component-library-r
 import React, { useState } from "react";
 
 export function MicrophoneActivityPreviewInner({ className }: { className?: string }): JSX.Element {
-    const [value, setValue] = useState(0);
-    useLocalAudioInputActivity((d) => setValue(d));
+    const [value, setValue] = useState<number>(0);
+    useLocalAudioInputActivity((d) =>
+        setValue((oldValue) => {
+            oldValue -= oldValue / 5;
+            oldValue += d / 5;
+
+            return oldValue;
+        })
+    );
     const id = useId();
 
     return (
         <FormControl className={className}>
             <FormLabel htmlFor={id}>Microphone activity</FormLabel>
-            <Progress id={id} sx={{ div: { transition: "all 0.01s" } }} value={value} min={0} max={1} />
+            <Progress id={id} sx={{ div: { transition: "all 0s" } }} value={value} min={0} max={1} />
         </FormControl>
     );
 }

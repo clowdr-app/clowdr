6d85b09e1eb06bce3248d7c2435c323a243622a9
Ed Nutting <github@ednutting.com>
2021-04-27 14:00:51 +0100

    Sponsor booths: Fix whitescreen when logo is not available

--- frontend/src/aspects/Conference/Attend/Room/RoomTitle.tsx
+++ frontend/src/aspects/Conference/Attend/Room/RoomTitle.tsx
@@ -8,39 +8,43 @@ import type { RoomPage_RoomDetailsFragment } from "../../../../generated/graphql
 
 export function RoomTitle({ roomDetails }: { roomDetails: RoomPage_RoomDetailsFragment }): JSX.Element {
     const sponsorLogoUrl = useMemo((): string | null => {
-        if (
-            !roomDetails.originatingContentGroup?.contentItems ||
-            !roomDetails.originatingContentGroup.contentItems.length
-        ) {
+        try {
+            if (
+                !roomDetails.originatingContentGroup?.contentItems ||
+                !roomDetails.originatingContentGroup.contentItems.length
+            ) {
+                return null;
+            }
+
+            const dataBlob = roomDetails.originatingContentGroup.contentItems[0].data;
+
+            if (!isContentItemDataBlob(dataBlob)) {
+                return null;
+            }
+
+            const contentItemDataBlob = dataBlob as ContentItemDataBlob;
+
+            const latestVersion = R.last(contentItemDataBlob);
+
+            if (!latestVersion) {
+                return null;
+            }
+
+            if (
+                latestVersion.data.type !== ContentType_Enum.ImageUrl &&
+                latestVersion.data.type !== ContentType_Enum.ImageFile
+            ) {
+                return null;
+            }
+
+            if (latestVersion.data.type === ContentType_Enum.ImageUrl) {
+                return latestVersion.data.url;
+            } else {
+                const { bucket, key } = new AmazonS3URI(latestVersion.data.s3Url);
+                return `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`;
+            }
+        } catch {
             return null;
-        }
-
-        const dataBlob = roomDetails.originatingContentGroup.contentItems[0].data;
-
-        if (!isContentItemDataBlob(dataBlob)) {
-            return null;
-        }
-
-        const contentItemDataBlob = dataBlob as ContentItemDataBlob;
-
-        const latestVersion = R.last(contentItemDataBlob);
-
-        if (!latestVersion) {
-            return null;
-        }
-
-        if (
-            latestVersion.data.type !== ContentType_Enum.ImageUrl &&
-            latestVersion.data.type !== ContentType_Enum.ImageFile
-        ) {
-            return null;
-        }
-
-        if (latestVersion.data.type === ContentType_Enum.ImageUrl) {
-            return latestVersion.data.url;
-        } else {
-            const { bucket, key } = new AmazonS3URI(latestVersion.data.s3Url);
-            return `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`;
         }
     }, [roomDetails.originatingContentGroup?.contentItems]);
 

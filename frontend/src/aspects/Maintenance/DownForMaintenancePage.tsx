e2a1f9d671e618e30d9f91e56b7fa63f8c79db12
Ross Gardiner <github@public.rossng.eu>
2021-05-03 23:38:00 +0100

    Correct maintenance page

--- frontend/src/aspects/Maintenance/DownForMaintenancePage.tsx
+++ frontend/src/aspects/Maintenance/DownForMaintenancePage.tsx
@@ -8,11 +8,10 @@ export default function DownForMaintenancePage(): JSX.Element {
             <GenericErrorPage heading="Offline for maintenance">
                 <VStack alignItems="flex-start" maxW={625}>
                     <Text fontSize="xl" lineHeight="revert" fontWeight="light">
-                        Clowdr is offline for maintenance starting 5pm UTC on 7th April for approximately 30 minutes.
+                        Clowdr is offline for maintenance ending 1am UTC on 4th May.
                     </Text>
                     <Text fontSize="xl" lineHeight="revert" fontWeight="light">
-                        This is while we make important updates to the platform to improve the chat and video
-                        experiences.
+                        This is while we make important updates to the platform.
                     </Text>
                 </VStack>
             </GenericErrorPage>

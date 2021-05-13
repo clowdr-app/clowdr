ae0df80ae1e729ff688b4d513e114ef43c051f4c
Ross Gardiner <github@public.rossng.eu>
2021-05-13 20:26:09 +0100

    🗃 Truncate ImmediateSwitch before adding conferenceId column

--- hasura/migrations/1620859526467_alter_table_video_ImmediateSwitch_add_column_conferenceId/up.sql
+++ hasura/migrations/1620859526467_alter_table_video_ImmediateSwitch_add_column_conferenceId/up.sql
@@ -1,2 +1,6 @@
-alter table "video"."ImmediateSwitch" add column "conferenceId" uuid
- not null;
+TRUNCATE "video"."ImmediateSwitch";
+
+alter table
+    "video"."ImmediateSwitch"
+add
+    column "conferenceId" uuid not null;

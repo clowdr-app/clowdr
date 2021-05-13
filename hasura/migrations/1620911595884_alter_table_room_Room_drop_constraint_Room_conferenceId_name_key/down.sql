fbee6a342c976bfbc681cbbe48afd0a6fc83318b
Ed Nutting <github@ednutting.com>
2021-05-13 14:14:23 +0100

    🗃 Drop unique key from room.Room: (conference, room name)

---
+++ hasura/migrations/1620911595884_alter_table_room_Room_drop_constraint_Room_conferenceId_name_key/down.sql
@@ -0,0 +1,1 @@
+alter table "room"."Room" add constraint "Room_conferenceId_name_key" unique ("conferenceId", "name");
---
+++ hasura/migrations/1620911595884_alter_table_room_Room_drop_constraint_Room_conferenceId_name_key/up.sql
@@ -0,0 +1,1 @@
+alter table "room"."Room" drop constraint "Room_conferenceId_name_key";

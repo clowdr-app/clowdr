dde5be29187d4f0aacaa9b2747c2398b674579f1
Ed Nutting <fork@ednutting.com>
2021-07-05 14:45:44 +0100

    🗃 Notify organisers via email when a message is reported

---
+++ services/actions/src/handlers/chat.ts
@@ -0,0 +1,77 @@
+import { gql } from "@apollo/client/core";
+import assert from "assert";
+import { FlagInserted_GetSupportAddressDocument } from "../generated/graphql";
+import { apolloClient } from "../graphqlClient";
+import { FlagData, Payload } from "../types/hasura/event";
+import { insertEmails } from "./email";
+
+gql`
+    query FlagInserted_GetSupportAddress($messageSId: uuid!) {
+        chat_Message(where: { sId: { _eq: $messageSId } }) {
+            chat {
+                conference {
+                    id
+                    name
+                    shortName
+                    slug
+
+                    configurations(where: { key: { _eq: "SUPPORT_ADDRESS" } }) {
+                        id
+                        key
+                        value
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export async function handleFlagInserted(data: Payload<FlagData>): Promise<void> {
+    const newFlag = data.event.data.new;
+    if (newFlag) {
+        const response = await apolloClient.query({
+            query: FlagInserted_GetSupportAddressDocument,
+            variables: {
+                messageSId: newFlag.messageSId,
+            },
+        });
+
+        assert(response.data?.chat_Message.length, "Chat message not retrieved");
+
+        const conference = response.data.chat_Message[0].chat.conference;
+        if (conference.configurations.length > 0) {
+            await insertEmails(
+                [
+                    {
+                        emailAddress: conference.configurations[0].value,
+                        reason: "chat_moderation_report",
+                        subject: "[HIGH PRIORITY] Chat message reported in " + conference.shortName,
+                        htmlContents: `<p>Dear ${conference.shortName} support,</p>
+<p>This is an automated email to let you know that a message has been reported
+in your conference and needs your attention. Please go to the 
+<a href="${process.env.FRONTEND_PROTOCOL}://${process.env.FRONTEND_DOMAIN}/conference/${conference.slug}/manage/chats/moderation">moderation hub</a>
+to view and resolve the report.</p>
+`,
+                    },
+                ],
+                conference.id
+            );
+        } else {
+            await insertEmails(
+                [
+                    {
+                        emailAddress: process.env.FAILURE_NOTIFICATIONS_EMAIL_ADDRESS,
+                        reason: "chat_moderation_report",
+                        subject: "[HIGH PRIORITY] Chat message reported in " + conference.shortName,
+                        htmlContents: `<p>This is an automated email. A message has been reported
+in ${conference.shortName} and needs attention. Please go to the 
+<a href="${process.env.FRONTEND_PROTOCOL}://${process.env.FRONTEND_DOMAIN}/conference/${conference.slug}/manage/chats/moderation">moderation hub</a>
+to view and resolve the report.</p>
+`,
+                    },
+                ],
+                conference.id
+            );
+        }
+    }
+}

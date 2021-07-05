e4bab6adb6296a9e60adb67b934335bd4f82098d
Ed Nutting <fork@ednutting.com>
2021-07-05 13:20:56 +0100

    🗃 New chat flags/reports: Send push/in-browser notifications

--- services/realtime/src/types/hasura.ts
+++ services/realtime/src/types/hasura.ts
@@ -44,6 +44,32 @@ export interface Pin {
     created_at: string;
 }
 
+export enum FlagType {
+    /** It's abusive or harmful. */
+    Abusive = "Abusive",
+    /** It contains false information and is intended to mislead readers. */
+    Disinformation = "Disinformation",
+    /** It gives the wrong idea or impression. */
+    Misleading = "Misleading",
+    /** It expresses intentions of self-harm or suicide. */
+    RiskToLife = "Risk_To_Life",
+    /** It's spam, suspicious or annoying. */
+    Spam = "Spam",
+}
+
+export interface Flag {
+    created_at: string;
+    discussionChatId?: string | null;
+    flaggedById?: string | null;
+    id: number;
+    messageSId: string;
+    notes?: string | null;
+    resolution?: string | null;
+    resolved_at?: string | null;
+    type: FlagType;
+    updated_at: string;
+}
+
 export interface PushNotificationSubscription {
     userId: string;
     endpoint: string;

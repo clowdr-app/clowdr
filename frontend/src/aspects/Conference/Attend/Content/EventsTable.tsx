8b83f15a63be9d240ec8c49717d08a42ca5d6588
Ed Nutting <github@ednutting.com>
2021-07-23 00:27:26 +0100

    Fix item page events table miscolouring

--- frontend/src/aspects/Conference/Attend/Content/EventsTable.tsx
+++ frontend/src/aspects/Conference/Attend/Content/EventsTable.tsx
@@ -91,7 +91,7 @@ function Event({
     }, [endMillis, now, startMillis]);
 
     return (
-        <Tr p={2} my={2} w="auto" backgroundColor={happeningSoonOrNow ? "purple.500" : "initial"}>
+        <Tr p={2} my={2} w="auto" backgroundColor={happeningSoonOrNow ? "green.500" : "initial"}>
             {maybeRegistrant ? (
                 <Td p={0} pl={1}>
                     <StarEventButton eventIds={itemEvent.id} />

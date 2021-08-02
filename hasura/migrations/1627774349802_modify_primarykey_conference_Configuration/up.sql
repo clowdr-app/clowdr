d066bcc45bb5afb5706328afead0a653bd5c758f
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2021-08-02 22:44:01 +0100

    🗃 Fix the last migration in the list

--- hasura/migrations/1627774349802_modify_primarykey_conference_Configuration/up.sql
+++ hasura/migrations/1627774349802_modify_primarykey_conference_Configuration/up.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-ALTER TABLE "conference"."Configuration" DROP CONSTRAINT "Configuration_pkey";
+ALTER TABLE "conference"."Configuration" DROP CONSTRAINT IF EXISTS "Configuration_pkey";
 
 ALTER TABLE "conference"."Configuration"
     ADD CONSTRAINT "Configuration_pkey" PRIMARY KEY ("key", "conferenceId");

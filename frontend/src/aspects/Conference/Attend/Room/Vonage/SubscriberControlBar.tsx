7a4680bb6f69fc5d22fe3f30b9f7c2175de04661
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2021-09-14 19:03:36 +0100

    Rename Kick to Remove

--- frontend/src/aspects/Conference/Attend/Room/Vonage/SubscriberControlBar.tsx
+++ frontend/src/aspects/Conference/Attend/Room/Vonage/SubscriberControlBar.tsx
@@ -72,7 +72,7 @@ export default function SubscriberControlBar({
                         }}
                         size="xs"
                     >
-                        Kick
+                        Remove
                     </Button>
                 ) : undefined}
             </ButtonGroup>

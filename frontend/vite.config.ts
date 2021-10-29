4b031f772ae0de7253f726effab37c2a8844640c
Ross Gardiner <github@public.rossng.eu>
2021-10-29 23:28:27 +0200

    Minify dev build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react()],
+    esbuild: {
+        minify: true,
+    },
     build: {
         rollupOptions: {
             plugins: [],

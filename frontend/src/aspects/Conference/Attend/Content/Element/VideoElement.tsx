276e53ccbf32bdae3b0f81c0e779ba8c4001da6c
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2021-11-03 21:14:03 +0000

    Add speed control for videos

--- frontend/src/aspects/Conference/Attend/Content/Element/VideoElement.tsx
+++ frontend/src/aspects/Conference/Attend/Content/Element/VideoElement.tsx
@@ -1,4 +1,15 @@
-import { Heading, Text } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Menu,
+    MenuButton,
+    MenuItemOption,
+    MenuList,
+    MenuOptionGroup,
+    Text,
+} from "@chakra-ui/react";
 import type { AudioElementBlob, VideoElementBlob } from "@clowdr-app/shared-types/build/content";
 import { WebVTTConverter } from "@clowdr-app/srt-webvtt";
 import AmazonS3URI from "amazon-s3-uri";
@@ -10,6 +21,7 @@ import type { Config } from "react-player";
 import ReactPlayer from "react-player";
 import type { TrackProps } from "react-player/file";
 import { Content_ElementType_Enum } from "../../../../../generated/graphql";
+import { FAIcon } from "../../../../Icons/FAIcon";
 import useTrackView from "../../../../Realtime/Analytics/useTrackView";
 
 export function VideoElement({
@@ -27,7 +39,7 @@ export function VideoElement({
     onPause?: () => void;
     onFinish?: () => void;
 }): JSX.Element {
-    const videoURL = useMemo(() => {
+    const { url: videoURL, isHLS } = useMemo(() => {
         let s3Url = "transcode" in elementData ? elementData.transcode?.s3Url : undefined;
 
         if (!s3Url && elementData.s3Url) {
@@ -35,11 +47,17 @@ export function VideoElement({
         }
 
         if (!s3Url) {
-            return undefined;
+            return { url: undefined, isHLS: false };
         }
         const { bucket, key } = new AmazonS3URI(s3Url);
 
-        return `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`;
+        return {
+            url:
+                key && bucket
+                    ? `https://s3.${import.meta.env.SNOWPACK_PUBLIC_AWS_REGION}.amazonaws.com/${bucket}/${key}`
+                    : undefined,
+            isHLS: !!key?.endsWith(".m3u8"),
+        };
     }, [elementData]);
 
     const {
@@ -73,23 +91,27 @@ export function VideoElement({
         if (loading) {
             return null;
         }
-        if (error || !subtitlesUrl) {
-            return {};
+
+        const tracks: TrackProps[] = [];
+        if (!error && subtitlesUrl) {
+            const track: TrackProps = {
+                kind: "subtitles",
+                src: subtitlesUrl,
+                srcLang: "en",
+                default: false,
+                label: "English",
+            };
+
+            tracks.push(track);
         }
-        const track: TrackProps = {
-            kind: "subtitles",
-            src: subtitlesUrl,
-            srcLang: "en",
-            default: false,
-            label: "English",
-        };
+
         const hlsOptions: Partial<HlsConfig> = {
             maxBufferLength: 0.05,
             maxBufferSize: 500,
         };
         return {
             file: {
-                tracks: [track],
+                tracks,
                 hlsVersion: "1.0.4",
                 hlsOptions,
             },
@@ -98,50 +120,94 @@ export function VideoElement({
 
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
     const playerRef = useRef<ReactPlayer | null>(null);
+    const [playbackRate, setPlaybackRate] = useState<number>(1);
     const player = useMemo(() => {
         // Only render the player once both the video URL and the subtitles config are available
         // react-player memoizes internally and only re-renders if the url or key props change.
         return !videoURL || !config ? undefined : (
-            <ReactPlayer
-                url={videoURL}
-                controls={true}
-                width="100%"
-                height="auto"
-                onEnded={() => {
-                    setIsPlaying(false);
-                }}
-                onError={() => {
-                    setIsPlaying(false);
-                }}
-                onPause={() => {
-                    setIsPlaying(false);
-                    onPause?.();
-                }}
-                onPlay={() => {
-                    setIsPlaying(true);
-                    onPlay?.();
-                    const hlsPlayer = playerRef.current?.getInternalPlayer("hls") as Hls;
-                    if (hlsPlayer) {
-                        hlsPlayer.config.maxBufferLength = 30;
-                        hlsPlayer.config.maxBufferSize = 60 * 1000 * 1000;
+            <Box>
+                <ReactPlayer
+                    url={videoURL}
+                    controls={true}
+                    width="100%"
+                    height="auto"
+                    onEnded={() => {
+                        setIsPlaying(false);
+                    }}
+                    onError={() => {
+                        setIsPlaying(false);
+                    }}
+                    onPause={() => {
+                        setIsPlaying(false);
+                        onPause?.();
+                    }}
+                    onPlay={() => {
+                        setIsPlaying(true);
+                        onPlay?.();
+                        const hlsPlayer = playerRef.current?.getInternalPlayer("hls") as Hls;
+                        if (hlsPlayer) {
+                            hlsPlayer.config.maxBufferLength = 30;
+                            hlsPlayer.config.maxBufferSize = 60 * 1000 * 1000;
+                        }
+                    }}
+                    onProgress={({ played }) => {
+                        if (played >= 1) {
+                            onFinish?.();
+                        }
+                    }}
+                    config={{ ...config }}
+                    ref={playerRef}
+                    style={
+                        isHLS
+                            ? { borderRadius: "10px", overflow: "hidden" }
+                            : { borderTopRightRadius: "10px", borderTopLeftRadius: "10px", overflow: "hidden" }
                     }
-                }}
-                onProgress={({ played }) => {
-                    if (played >= 1) {
-                        onFinish?.();
-                    }
-                }}
-                config={{ ...config }}
-                ref={playerRef}
-                style={{ borderRadius: "10px", overflow: "hidden" }}
-            />
+                    playbackRate={playbackRate}
+                />
+                {!isHLS ? (
+                    <Flex borderBottomRadius="2xl" p={1} justifyContent="flex-end">
+                        {/* <SpeedControl
+                            options={["0.5x", "0.75x", "1x", "1.2x", "1.5x", "2x"]}
+                            onChange={(value) => setPlaybackRate(parseFloat(value))}
+                        /> */}
+                        <Menu>
+                            <MenuButton as={Button} size="xs">
+                                Speed <FAIcon iconStyle="s" icon="chevron-down" />
+                            </MenuButton>
+                            <MenuList size="xs" spacing="compact">
+                                <MenuOptionGroup
+                                    onChange={(value) => {
+                                        const v = parseFloat(value as string);
+                                        if (Number.isFinite(v)) {
+                                            setPlaybackRate(v);
+                                        } else {
+                                            setPlaybackRate(1);
+                                        }
+                                    }}
+                                    type="radio"
+                                    value={playbackRate.toFixed(2)}
+                                >
+                                    <MenuItemOption value="0.50">0.5x</MenuItemOption>
+                                    <MenuItemOption value="0.75">0.75x</MenuItemOption>
+                                    <MenuItemOption value="1.00">1x</MenuItemOption>
+                                    <MenuItemOption value="1.20">1.2x</MenuItemOption>
+                                    <MenuItemOption value="1.50">1.5x</MenuItemOption>
+                                    <MenuItemOption value="2.00">2x</MenuItemOption>
+                                </MenuOptionGroup>
+                            </MenuList>
+                        </Menu>
+                    </Flex>
+                ) : undefined}
+            </Box>
         );
-    }, [videoURL, config, onPause, onPlay, onFinish]);
+    }, [videoURL, config, isHLS, playbackRate, onPause, onPlay, onFinish]);
 
     useEffect(() => {
         if (playerRef.current) {
             const hls: Hls = playerRef.current.getInternalPlayer("hls") as Hls;
-            hls.subtitleDisplay = false;
+            if (hls) {
+                hls.subtitleDisplay = false;
+            }
         }
     }, []);
 
@@ -167,3 +233,54 @@ function TrackVideoView({ elementId, isPlaying }: { elementId: string; isPlaying
     useTrackView(isPlaying, elementId, "Element");
     return <></>;
 }
+
+// function SpeedControl({ onChange, options }: { options: string[]; onChange: (value: string) => void }) {
+//     const { getRootProps, getRadioProps } = useRadioGroup({
+//         name: "framework",
+//         defaultValue: "react",
+//         onChange,
+//     });
+
+//     const group: any = getRootProps();
+//     return (
+//         <ButtonGroup {...group} spacing={0} isAttached>
+//             {options.map((value) => {
+//                 const radio = getRadioProps({ value });
+//                 return (
+//                     <SpeedControlRadioCard key={value} {...radio}>
+//                         {value}
+//                     </SpeedControlRadioCard>
+//                 );
+//             })}
+//         </ButtonGroup>
+//     );
+// }
+
+// function SpeedControlRadioCard(props: React.PropsWithChildren<UseRadioProps>) {
+//     const { getInputProps, getCheckboxProps } = useRadio(props);
+
+//     const input: any = getInputProps();
+//     const checkbox: any = getCheckboxProps();
+
+//     return (
+//         <Box as="label">
+//             <input {...input} />
+//             <Box
+//                 {...checkbox}
+//                 cursor="pointer"
+//                 boxShadow="md"
+//                 _checked={{
+//                     bg: "PrimaryActionButton.500",
+//                     color: "white",
+//                 }}
+//                 _focus={{
+//                     boxShadow: "outline",
+//                 }}
+//                 px={1}
+//                 py={0}
+//             >
+//                 {props.children}
+//             </Box>
+//         </Box>
+//     );
+// }

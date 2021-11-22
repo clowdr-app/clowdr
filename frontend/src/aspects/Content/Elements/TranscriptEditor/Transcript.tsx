5989f1a60a4e8fa5f3e4452c2db55230530e7526
Nick Guerette <nick.guerette@midspace.app>
2021-11-21 18:27:18 -0800

    Add timecode input validation back in

--- frontend/src/aspects/Content/Elements/TranscriptEditor/Transcript.tsx
+++ frontend/src/aspects/Content/Elements/TranscriptEditor/Transcript.tsx
@@ -3,15 +3,7 @@ import { FixedSizeList } from "react-window";
 import type { SubtitlesArray } from "./srt";
 import SubtitleBlock from "./SubtitleBlock";
 
-function validateNewStartTenths({
-    oldTranscript,
-    index,
-    newStartTenths,
-}: {
-    oldTranscript: SubtitlesArray;
-    index: number;
-    newStartTenths: number;
-}): Boolean {
+function validateNewStartTenths(oldTranscript: SubtitlesArray, index: number, newStartTenths: number): Boolean {
     if (newStartTenths < oldTranscript[index].startTenths) {
         if (index === 0) return newStartTenths >= 0;
         return newStartTenths >= oldTranscript[index - 1].endTenths;
@@ -20,15 +12,7 @@ function validateNewStartTenths({
     return newStartTenths < oldTranscript[index].endTenths;
 }
 
-function validateNewEndTenths({
-    oldTranscript,
-    index,
-    newEndTenths,
-}: {
-    oldTranscript: SubtitlesArray;
-    index: number;
-    newEndTenths: number;
-}): Boolean {
+function validateNewEndTenths(oldTranscript: SubtitlesArray, index: number, newEndTenths: number): Boolean {
     if (newEndTenths > oldTranscript[index].endTenths) {
         if (index === oldTranscript.length) return true; // TODO upper limit is end of video
         return newEndTenths <= oldTranscript[index + 1].startTenths;
@@ -47,14 +31,6 @@ export default function Transcript({
     function SubtitleBlockJITRenderer({ index, style }: { index: number; style: CSSProperties }): JSX.Element {
         const { startTenths, endTenths, text } = value[index];
 
-        function stateChange(updates: { startTenths?: number; endTenths?: number; text?: string }): void {
-            onInput((oldTranscript) => [
-                ...oldTranscript.slice(0, index),
-                { startTenths, endTenths, text, ...updates },
-                ...oldTranscript.slice(index + 1),
-            ]);
-        }
-
         return (
             <SubtitleBlock
                 {...{
@@ -63,9 +39,32 @@ export default function Transcript({
                     endTenths,
                     text,
                     style,
-                    onTextInput: (newText) => stateChange({ text: newText }),
-                    onStartTenthsInput: (newStartTenths) => stateChange({ startTenths: newStartTenths }),
-                    onEndTenthsInput: (newEndTenths) => stateChange({ endTenths: newEndTenths }),
+                    onTextInput: (newText) =>
+                        onInput((oldTranscript) => [
+                            ...oldTranscript.slice(0, index),
+                            { startTenths, endTenths, text: newText },
+                            ...oldTranscript.slice(index + 1),
+                        ]),
+                    onStartTenthsInput: (newStartTenths) =>
+                        onInput((oldTranscript) =>
+                            validateNewStartTenths(oldTranscript, index, newStartTenths)
+                                ? [
+                                      ...oldTranscript.slice(0, index),
+                                      { startTenths: newStartTenths, endTenths, text },
+                                      ...oldTranscript.slice(index + 1),
+                                  ]
+                                : oldTranscript
+                        ),
+                    onEndTenthsInput: (newEndTenths) =>
+                        onInput((oldTranscript) =>
+                            validateNewEndTenths(oldTranscript, index, newEndTenths)
+                                ? [
+                                      ...oldTranscript.slice(0, index),
+                                      { startTenths, endTenths: newEndTenths, text },
+                                      ...oldTranscript.slice(index + 1),
+                                  ]
+                                : oldTranscript
+                        ),
                 }}
             />
         );

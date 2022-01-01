0c5a9ec49a001a6e6a98aee188b79838dea24d5b
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2022-01-01 23:08:55 +0000

    🗃 Remove silly test foreign key

--- hasura/migrations/default/1638365540053_set_fk_chat_Subscription_registrantId_chatId/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "chat"."Subscription" drop constraint "Subscription_registrantId_chatId_fkey";
--- hasura/migrations/default/1638365540053_set_fk_chat_Subscription_registrantId_chatId/up.sql
+++
@@ -1,5 +0,0 @@
-alter table "chat"."Subscription"
-  add constraint "Subscription_registrantId_chatId_fkey"
-  foreign key ("registrantId", "chatId")
-  references "chat"."Pin"
-  ("registrantId", "chatId") on update restrict on delete restrict;

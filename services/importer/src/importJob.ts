4b284bfe5e756db5727ddd3fc242ffc59c0dfcda
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2022-04-16 16:16:53 +0100

    When an import initialize hook request fails, mark the job as failed

--- services/importer/src/importJob.ts
+++ services/importer/src/importJob.ts
@@ -5,6 +5,7 @@ import type { Request, Response } from "express";
 import express from "express";
 import { assertType } from "typescript-is";
 import { publishTask } from "./rabbitmq/tasks";
+import { appendJobErrors } from "./tasks/lib/job";
 import type { ImportJob } from "./types/job";
 
 export const router = express.Router();
@@ -24,8 +25,27 @@ router.post("/inserted", json({ limit: "5mb" }), async (req: Request, res: Respo
 
         res.status(200).json("OK");
     } catch (err) {
-        req.log.error({ err, body: req.body }, "Failure while handling import job inserted event");
+        req.log.error({ err }, "Failure while handling import job inserted event");
         res.status(500).json("Failure while handling import job inserted event");
+
+        try {
+            if (req?.body?.event?.data?.new?.id) {
+                await appendJobErrors(
+                    req.body.event.data.new.id,
+                    [
+                        {
+                            message: JSON.stringify(err),
+                        },
+                    ],
+                    true
+                );
+            } else {
+                req.log.error({}, "Unable to mark import job as failed - no id.");
+            }
+        } catch (err2) {
+            req.log.error({ err: err2 }, "Unable to mark import job as failed - unexpected error.");
+        }
+
         return;
     }
 });

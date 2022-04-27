babddd50ba6708c04a963e6ba4a0c6f93789bf43
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2022-04-27 11:17:01 +0100

    Fix page titles

--- frontend/src/aspects/Hooks/useTitle.tsx
+++ frontend/src/aspects/Hooks/useTitle.tsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async";
 export function useTitle(text: string): JSX.Element {
     return (
         <Helmet>
-            <title>{text.length > 0 ? `${text}: ` : ""} - Midspace</title>
+            <title>{text.length > 0 ? `${text} - ` : ""}Midspace</title>
         </Helmet>
     );
 }

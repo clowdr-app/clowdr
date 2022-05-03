c843fd92f197cbdef3b6605f929ac46130a7f12f
Ross Gardiner <github@public.rossng.eu>
2022-05-03 23:52:42 +0200

    📦 Install some missing packages; fix express-jwt integration with jwks-rsa

--- packages/node/auth/src/middlewares/checkJwt.ts
+++ packages/node/auth/src/middlewares/checkJwt.ts
@@ -1,19 +1,33 @@
-import jwt from "express-jwt";
+import { Request, RequestHandler } from "express";
+import { expressjwt as jwt } from "express-jwt";
+import { Jwt, Secret } from "jsonwebtoken";
 import jwksRsa from "jwks-rsa";
 
 // Authorization middleware. When used, the
 // Access Token must exist and be verified against
 // the Auth0 JSON Web Key Set
-export const checkJwt = jwt({
+export const checkJwt: RequestHandler = jwt({
     // Dynamically provide a signing key
     // based on the kid in the header and
     // the signing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 1,
-        jwksUri: `https://${process.env.AUTH0_API_DOMAIN}/.well-known/jwks.json`,
-    }),
+    secret: (req: Request, token: Jwt | undefined) =>
+        new Promise<Secret>((resolve, reject) => {
+            const getter = jwksRsa.expressJwtSecret({
+                cache: true,
+                rateLimit: true,
+                jwksRequestsPerMinute: 1,
+                jwksUri: `https://${process.env.AUTH0_API_DOMAIN}/.well-known/jwks.json`,
+            });
+            getter(req, token?.header, token?.payload, (err: any, secret?: Secret | undefined) => {
+                if (err) {
+                    reject(err);
+                }
+                if (secret) {
+                    resolve(secret);
+                }
+                reject("No secret returned");
+            });
+        }),
 
     // Validate the audience and the issuer.
     audience: process.env.AUTH0_AUDIENCE,
@@ -22,9 +36,9 @@ export const checkJwt = jwt({
     getToken: function fromHeaderOrQuerystring(req) {
         if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
             return req.headers.authorization.split(" ")[1];
-        } else if (req.query && req.query.token) {
+        } else if (req.query && req.query.token && typeof req.query.token === "string") {
             return req.query.token;
         }
-        return null;
+        return undefined;
     },
 });

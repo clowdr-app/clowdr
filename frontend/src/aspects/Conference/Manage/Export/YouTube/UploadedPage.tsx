56ed3b8c52611ddef5bf29b6990543b2456ce723
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2022-05-26 16:17:10 +0100

    Uploaded videos page: Secondary order by updated at, to try to select ongoing jobs first

--- frontend/src/aspects/Conference/Manage/Export/YouTube/UploadedPage.tsx
+++ frontend/src/aspects/Conference/Manage/Export/YouTube/UploadedPage.tsx
@@ -41,7 +41,7 @@ gql`
     ) {
         job_queues_UploadYouTubeVideoJob(
             where: { conferenceId: { _eq: $conferenceId }, subconferenceId: $subconferenceCond }
-            order_by: { createdAt: desc }
+            order_by: [{ createdAt: desc }, { updatedAt: asc }]
             limit: 100
         ) {
             ...UploadYouTubeVideos_UploadYouTubeVideoJob
--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -56077,7 +56077,7 @@ export const UploadYouTubeVideos_GetUploadYouTubeVideoJobsDocument = gql`
     ) {
         job_queues_UploadYouTubeVideoJob(
             where: { conferenceId: { _eq: $conferenceId }, subconferenceId: $subconferenceCond }
-            order_by: { createdAt: desc }
+            order_by: [{ createdAt: desc }, { updatedAt: asc }]
             limit: 100
         ) {
             ...UploadYouTubeVideos_UploadYouTubeVideoJob
